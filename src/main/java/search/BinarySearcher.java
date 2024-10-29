27ab5a02aa93b6b28acc5d19cb2986dde16a8b60
MaryS <masha.37@mail.ru>
2024-10-29 18:56:31 +0300

    choosing of file added

--- src/main/java/search/BinarySearcher.java
+++ src/main/java/search/BinarySearcher.java
@@ -1,5 +1,5 @@
 package main.java.search;
-
+import java.util.ArrayList;
 import main.java.entity.Sortable;
 
 public class BinarySearcher {
@@ -13,8 +13,11 @@ public class BinarySearcher {
 
         int middle=leftBorder+((rightBorder-leftBorder)/2);
 
+        if((middle==leftBorder)&&(element!=array.get(middle))) return -1;
+        else if(array.get(middle).compareTo(element)>0){
             return binarySearch(array,leftBorder,middle,element);
         }else if(array.get(middle).compareTo(element)<0){
             return  binarySearch(array,middle,rightBorder,element);
+        }else    return middle;
     }
 }
